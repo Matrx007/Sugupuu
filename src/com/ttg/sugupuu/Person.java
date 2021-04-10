9df587badb69e118fbdc8f702e1b0e769b2624d7
Matrx007 <raintz1000@gmail.com>
2021-04-10 11:13:11 +0300

    Made fields public

--- src/com/ttg/sugupuu/Person.java
+++ src/com/ttg/sugupuu/Person.java
@@ -3,10 +3,10 @@ package com.ttg.sugupuu;
 import java.util.ArrayList;
 
 public class Person {
-    String name;
-    int birth_year;
+    public String name;
+    public int birth_year;
 
-    Person parent1;
-    Person parent2;
-    ArrayList<Person> children;
+    public Person parent1;
+    public Person parent2;
+    public ArrayList<Person> children;
 }
