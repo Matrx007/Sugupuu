6b8079b67854fae1315f275149f25cdffddfba92
GregorSomething <gregor.suurvarik@gmail.com>
2021-04-10 13:11:27 +0300

    Update Comparative.java

--- src/com/ttg/sugupuu/algorithms/Comparative.java
+++ src/com/ttg/sugupuu/algorithms/Comparative.java
@@ -3,6 +3,7 @@ package com.ttg.sugupuu.algorithms;
 import com.ttg.sugupuu.Person;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Comparative {
 
@@ -47,4 +48,19 @@ public class Comparative {
         });
         return siblings;
     }
+
+    public static ArrayList<Person> getSiblings(Person person, ArrayList<Person> roots, boolean isMale) {
+        ArrayList<Person> siblings = new ArrayList<>();
+        getSiblings(person, roots).forEach(sibling -> {
+            if (sibling.isMale == isMale) siblings.add(sibling);
+        });
+        return siblings;
+    }
+
+    public static ArrayList<ArrayList<Person>> getGrandparents(Person person, ArrayList<Person> roots) {
+        ArrayList<ArrayList<Person>> grandparents = new ArrayList<>();
+        findParents(person, roots).forEach(parent -> grandparents.add(findParents(parent, roots)));
+        return grandparents;
+    }
+
 }
