899053adfe073b41054332c0194bd572ec5c6bd9
GregorSomething <gregor.suurvarik@gmail.com>
2021-04-10 11:47:41 +0300

    Update Main.java

--- src/com/ttg/sugupuu/Main.java
+++ src/com/ttg/sugupuu/Main.java
@@ -6,6 +6,5 @@ import processing.core.PConstants;
 public class Main {
     public static void main(String[] args) {
         Game.createGame(1366, 768, new GUI(), 60f, PConstants.P2D);
-        SaveFamilytree.save();
     }
 }
