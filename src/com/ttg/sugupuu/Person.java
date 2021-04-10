4252ef2a69b4dca51d4e28159e9b9fabe0fcbac5
GregorSomething <gregor.suurvarik@gmail.com>
2021-04-10 11:34:46 +0300

    Save things

--- src/com/ttg/sugupuu/Person.java
+++ src/com/ttg/sugupuu/Person.java
@@ -2,11 +2,16 @@ package com.ttg.sugupuu;
 
 import java.util.ArrayList;
 
-public class Person {
-    String name;
-    int birth_year;
+public class Person implements java.io.Serializable {
 
-    Person parent1;
-    Person parent2;
-    ArrayList<Person> children;
+    public Person(String name, int birth_year, ArrayList<Person> children) {
+        this.name = name;
+        this.birth_year = birth_year;
+        this.children = children;
+    }
+    private static final long serialVersionUID = 1L;
+
+    public String name;
+    public int birth_year;
+    public ArrayList<Person> children;
 }
